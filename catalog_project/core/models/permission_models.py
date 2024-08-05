40b8341c37ddb4f3fcde0e5d3bd2579f6459b663
Allysonfreitas210695 <alisonfr83@gmail.com>
2024-08-05 19:33:47 -0300

    chore: update entities permission

--- catalog_project/core/models/permission_models.py
+++ catalog_project/core/models/permission_models.py
@@ -1,20 +1,16 @@
 from django.db import models
 from .base_model import BaseModel
-from .company_models import Company
 from .user_models import User
-from core.validators.category_validators import validate_category_name
+from core.validators.permission_validators import validate_permission_name
 
 class Permission(BaseModel):
     name = models.CharField(
         max_length=255,
         null=False,
         blank=False,
-        validators=[validate_category_name]
+        validators=[validate_permission_name]
     )
-
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='permission_users')
-
-
     class Meta:
         verbose_name = "Permission"
         verbose_name_plural = "Permissions"
