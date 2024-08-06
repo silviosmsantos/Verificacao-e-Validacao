aed68525be9c1e53a6a7f159d2fde836c03c68eb
Allysonfreitas210695 <alisonfr83@gmail.com>
2024-08-05 23:59:33 -0300

    feat: add models __init__ for message

--- catalog_project/core/models/__init__.py
+++ catalog_project/core/models/__init__.py
@@ -6,3 +6,5 @@ from .company_models import Company
 from .category_models import Category
 from .permission_models import Permission
 from .audit_log import AuditLog
+from .catalog_models import Catalog
+from .message_models import Message
