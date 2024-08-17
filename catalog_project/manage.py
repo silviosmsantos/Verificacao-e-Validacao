c92027b3a905966cf1b2f1bf19af47e7f2d0939c
Allysonfreitas210695 <alisonfr83@gmail.com>
2024-08-17 08:28:44 -0300

    feat: add configuration system create file media

--- catalog_project/manage.py
+++ catalog_project/manage.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+def ensure_media_directory():
+    media_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media')
+    if not os.path.exists(media_root):
+        os.makedirs(media_root)
+        print(f'Pasta {media_root} criada com sucesso.')
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catalog_project.settings')
+    ensure_media_directory()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
