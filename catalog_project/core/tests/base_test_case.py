4ffe5336abb3d98d0f4b33416e4f14be66382b96
Allysonfreitas210695 <alisonfr83@gmail.com>
2024-08-17 11:50:31 -0300

    chore: update entite product

--- catalog_project/core/tests/base_test_case.py
+++ catalog_project/core/tests/base_test_case.py
@@ -2,6 +2,11 @@ from django.test import TestCase
 from core.models.company_models import Company
 from core.models.user_models import User
 from core.models.catalog_models import Catalog
+from core.models.category_models import Category
+from core.models.product_models import Product
+from io import BytesIO
+from PIL import Image
+from django.core.files.base import ContentFile
 
 class BaseTestCase(TestCase):
     def setUp(self):
@@ -9,6 +14,8 @@ class BaseTestCase(TestCase):
         self.company = self.create_company()
         self.user = self.create_user(self.company)
         self.catalog = self.create_catalog(self.company, self.user)
+        self.category = self.create_category()
+        self.product_data = self.get_product_data()
 
     def create_company(self):
         return Company.objects.create(name='Test Company', status='active')
@@ -31,6 +38,27 @@ class BaseTestCase(TestCase):
             company=company,
             user=user
         )
+
+    def create_category(self):
+        return Category.objects.create(name='Test Category', company=self.company)
+
+    def create_test_image(self):
+        image = BytesIO()
+        img = Image.new('RGB', (100, 100), color=(73, 109, 137))
+        img.save(image, 'PNG')
+        image.seek(0)
+        return ContentFile(image.read(), 'test_image.png')
+
+    def get_product_data(self):
+        return {
+            'name': 'Test Product',
+            'description': 'Test Description',
+            'price': 9.99,
+            'image': self.create_test_image(),
+            'status': 'active',
+            'category': self.category,
+            'catalog': self.catalog
+        }
 
     def get_message_data(self, catalog=None, name='Client Name', email='client@example.com', phone='1234567890', content='This is a test message'):
         catalog = catalog or self.catalog
